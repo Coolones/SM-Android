094ca4f651feefdf08c2c793869662c5d88be43e
Jasper van Son <319710@student.fontys.nl>
2016-09-11 20:09:40 +0200

    Opdrachten week 2 af

---
+++ CSI_week_1/app/src/main/java/org/fhict/csi_week_1/MainActivity.java
@@ -0,0 +1,25 @@
+package org.fhict.csi_week_1;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.Report);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), ReportActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
